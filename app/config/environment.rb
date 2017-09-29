96ed405ac809ffe0620304c79da7801f5dfe33c2
@snowman <jimmoffitt@yahoo.com>
2017-09-29 13:12:56 -0600

    Update environment.rb

--- app/config/environment.rb
+++ app/config/environment.rb
@@ -5,21 +5,21 @@ Bundler.require
 APP_ROOT = File.expand_path("..", __dir__)
 
 # require the controller(s)
-Dir.glob(File.join(APP_ROOT, 'app', 'controllers', '*.rb')).each { |file| require file }
+Dir.glob(File.join(APP_ROOT, 'controllers', '*.rb')).each { |file| require file }
 
 # require the helper(s)
-Dir.glob(File.join(APP_ROOT, 'app', 'helpers', '*.rb')).each { |file| require file }
+Dir.glob(File.join(APP_ROOT, 'helpers', '*.rb')).each { |file| require file }
 
 # require database configurations
 #require File.join(APP_ROOT, 'config', 'database')
 
 # configure TaskManagerApp settings
 class Application < Sinatra::Base
-	set :method_override, true
-	set :root, APP_ROOT
-	
-	puts "APP_ROOT: #{APP_ROOT}"
-	
-	#set :views, File.join(APP_ROOT, "app", "views")
-	#set :public_folder, File.join(APP_ROOT, "app", "public")
+  set :method_override, true
+  set :root, APP_ROOT
+
+  #puts "APP_ROOT: #{APP_ROOT}"
+
+  #set :views, File.join(APP_ROOT, "app", "views")
+  #set :public_folder, File.join(APP_ROOT, "app", "public")
 end
